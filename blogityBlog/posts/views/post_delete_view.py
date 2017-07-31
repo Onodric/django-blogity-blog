77892f7e72de76745e0c0d3052c29b1f9d429a36
Belve D. Marks <belvedmarks4@gmail.com>
2017-07-31 09:03:48 -0500

    Finished delete view and template

--- blogityBlog/posts/views/post_delete_view.py
+++ blogityBlog/posts/views/post_delete_view.py
@@ -1,16 +1,21 @@
 from ..models.posts import Posts
-from django.views.generic.base import TemplateView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
 
-class PostDeleteView(TemplateView):
+class PostDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     """
-    View for displaying the post lists
+    View for deleting a specific post
 
     Author: Ben Marks
     """
 
+    model = Posts
     template_name = 'blogityBlog/post_delete.html'
 
-    def get_context_data(self, **kwargs):
-        post_list = Posts.objects.filter(author=kwargs['pk'])
-        return {'post_list': post_list}
+    def get_success_url(self):
+        return reverse_lazy('posts:post_list')
+
+    success_message = "Post successfully DELETED"
--- blogityBlog/posts/views/post_update_view.py
+++ blogityBlog/posts/views/post_update_view.py
@@ -2,9 +2,10 @@ from ..models.posts import Posts
 from django.views.generic.edit import UpdateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     """
     View for editing a specific post
 
@@ -23,3 +24,5 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('posts:post_detail', kwargs={'pk': self.object.id})
+
+    success_message = "Post successfully UPDATED"
